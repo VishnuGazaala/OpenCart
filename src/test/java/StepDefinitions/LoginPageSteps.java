959da5d286ad1cfcd1d12209419b7743b867299b
VishnuGazzala <vgazzala@gmail.com>
2024-03-18 15:19:12 +0000

    first commit

--- src/test/java/StepDefinitions/LoginPageSteps.java
+++ src/test/java/StepDefinitions/LoginPageSteps.java
@@ -7,10 +7,10 @@ import io.cucumber.java.en.*;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
 
 import java.io.FileInputStream;
 import java.io.IOException;
